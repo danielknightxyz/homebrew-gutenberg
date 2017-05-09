243e3d31ebe816e05540a31eca356109fa014e24
Daniel Knight <daniel.knight@gmail.com>
2017-05-09 15:46:31 -0500

    updated sha for file.

--- gutenberg.rb
+++ gutenberg.rb
@@ -3,7 +3,7 @@ class Gutenberg < Formula
     desc "A template and scaffolding utility." 
     homepage "https://github.com/sourcefoundryus/gutenberg"
     url "https://github.com/sourcefoundryus/gutenberg/releases/download/0.1.0-beta.1/gutenberg-0.1.0-beta-1.zip"
-    sha256 "3e9b3aba042545826b6490adeb4ba37dcc654b61a1e5e53d3f6259c0016485df"
+    sha256 "57a4add1b12b2f351b65533faa859659fa9e4da1fbff2470e5ac0c71e30619c7"
     version "0.1.0-beta.1"
 
     bottle :unneeded
