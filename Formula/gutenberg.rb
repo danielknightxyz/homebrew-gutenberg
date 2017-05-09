cb1fcb719c66c9980b12714038ddaa855c32d6c0
Daniel Knight <daniel.knight@creditcards.com>
2017-05-09 14:25:54 -0500

    updated forumla.

--- Formula/gutenberg.rb
+++ Formula/gutenberg.rb
@@ -2,8 +2,8 @@
 class Gutenberg < Formula
     desc "A template and scaffolding utility." 
     homepage "https://github.com/sourcefoundryus/gutenberg"
-    url ""
-    sha256 ""
+    url "https://github.com/sourcefoundryus/homebrew-gutenberg/blob/master/archive/gutenberg-0.1.0-beta-1.tar"
+    sha256 "20408e6c2dbba6e9f1d75dad479f505fca843d96a8f89d7d879876de4bc4ebbf"
     version "1.0.0"
 
     bottle :unneeded
