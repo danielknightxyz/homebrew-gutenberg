a874733ef81dd7a56cdfd168c52bb7baaa3dcad0
Daniel Knight <daniel.knight@creditcards.com>
2017-05-09 14:25:01 -0500

    New forumla with new archive.

---
+++ Formula/gutenberg.rb
@@ -0,0 +1,14 @@
+
+class Gutenberg < Formula
+    desc "A template and scaffolding utility." 
+    homepage "https://github.com/sourcefoundryus/gutenberg"
+    url ""
+    sha256 ""
+    version "1.0.0"
+
+    bottle :unneeded
+
+    def install
+        bin.install "gutenberg"
+    end
+end
