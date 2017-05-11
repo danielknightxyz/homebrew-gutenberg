f537a0a552582c1a2e633103bb28046899463583
Daniel Knight <daniel.knight@gmail.com>
2017-05-10 20:39:49 -0500

    update to new version.

--- gutenberg.rb
+++ gutenberg.rb
@@ -2,14 +2,14 @@
 class Gutenberg < Formula
     desc "A template and scaffolding utility." 
     homepage "https://github.com/sourcefoundryus/gutenberg"
-    url "https://github.com/sourcefoundryus/gutenberg/releases/download/0.1.0-beta.1/gutenberg-0.1.0-beta-1.zip"
-    sha256 "9b7acf2f4ca660a1bfabe3f0d128899f2130f362989a94f2a13267741bec88d3"
-    version "0.1.0-beta.1"
+    url "https://github.com/sourcefoundryus/gutenberg/releases/download/0.1.0-beta.2/gutenberg-0.1.0-beta-2.zip"
+    sha256 "5aab3a044eff4ecffd6e1d03fd31e78dc1b1507b6cde7b7ba5b0cebc3f54cdbf"
+    version "0.1.0-beta.2"
 
     bottle :unneeded
 
     def install
-        lib.install "gutenberg-0.1.0-beta-1.jar"
+        lib.install "gutenberg-0.1.0-beta-2.jar"
         bin.install "gutenberg"
     end 
 end
